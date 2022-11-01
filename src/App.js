79187491b07a5b2368461aa87b02cb4984cbd7ec
Ramkumar <100298314+RAMKUMARPAZHANIVEL@users.noreply.github.com>
2022-11-01 20:54:20 +0530

    final commit
    final commit of jsx

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App main-container">
+         <h2>About us</h2>
+           <p>Id commodo adipisicing laboris do duis veniam.</p>
+         
+           <p>Aliqua velit et culpa cupidatat voluptate. Deserunt id esse veniam eiusmod pariatur nulla nostrud aliqua officia duis quis. Veniam commodo qui ex enim et dolore enim esse adipisicing est culpa velit ipsum sint.
+
+            Ut ullamco irure dolore excepteur eiusmod aliquip exercitation in in culpa sunt adipisicing. Amet nulla qui aute dolor. Consectetur minim eiusmod ut id reprehenderit duis in aliquip eiusmod. Veniam eu consectetur fugiat excepteur eiusmod incididunt cupidatat labore est id id incididunt.
+
+            Nisi et occaecat incididunt cillum nulla duis reprehenderit. Non culpa pariatur culpa id nulla voluptate minim eiusmod.
+            </p>
+            <p>Irure enim proident laborum anim labore aute duis aliquip ea dolore velit dolore
+           magna. Cillum fugiat Lorem ex labore mollit fugiat sunt veniam aliquip ea nostrud. 
+           Culpa dolore duis commodo id esse aute cupidatat.
+           </p>
+    </div>
+  );
+}
+export default App;
